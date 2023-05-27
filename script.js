0b4eb0f5bbf77e2ea9594a0c5fbc735eea09b701
Чимит <132557075+CHIMIT2023@users.noreply.github.com>
2023-05-27 18:41:36 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,67 @@
-let myAge=27
 
 
-const myName = `Chimit
-Lykshitov
-occupation
-training to web designs
-my hobby play table tennis
+let Shashljk={
+name:"Shashljk",
+ingredients:"Meat,onions,salt,pepper,mayonnaise",
+price:1500
+}
+let buuzy={
+name:"buuzy",
+ingredients:"beef,pork,onions,salt,pepper,dough",
+price:500
+};
 
-${2023-1995}
+let dish = [Shashljk,buuzy];
+alert(dish.length);
+console.log(dish);
 
+//удаление одного блюда//
+dish.shift();
+alert (dish[0]);
+console.log(alert);
+// добавление этого блюда обратно на первое место//
+let dish1 = ['buuzy'];
+alert(dish1[0]); 
+dish1.unshift({
+name:"Shashljk",
+ingredients:"Meat,salt,pepper,mayonnaise",
+price:1500}
+);
+alert(dish1[0]); 
+console.log(dish1);
+//приготовление блюдо по времени//
+let dish2 = {
+menu:[
+    {name:"Shashljk",
+    time:"60 min",
+    price:1500},
+    {name:"buuzy",
+    time:"30 min",    
+    price:500},
+]
+}
+console.log(dish2);
+//выручка//
+let dish4 = {
+ many:[
+     {name:"Shashljk",
+     revenue:"1500",
+     currency:"RUB",
+    },
+     {name:"buuzy",
+     revenue:"500",
+     currency:"RUB",
+    },
+]
+}
+console.log(dish4);
+console.log(1500+500);
+//удаление всех элементов кроме одного//
+let dish5 = ['Shashljk', 'buuzy'];
 
+dish5.pop();
 
-${myAge}
+alert(dish5[1]) 
+console.log(dish5);
+//конец//
 
-`
-
-console.log(myName);
